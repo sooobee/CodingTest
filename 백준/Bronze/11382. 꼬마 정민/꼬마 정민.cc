bae873e87c4ae7f6507abc2a2b752ebd6d1d705e
SOOBEE <20subi@jbnu.ac.kr>
2023-10-13 17:58:25 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(int argc, const char * argv[]) {
+    long long a, b, c;
+    
+    cin >> a >> b >> c;
+    
+    cout << (long long) (a+b+c) << endl;
+}
