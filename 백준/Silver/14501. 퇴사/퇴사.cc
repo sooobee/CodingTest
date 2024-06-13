494908526de44a7c826f334c3c537fb7666cd900
sooobee <20subi@jbnu.ac.kr>
2024-06-13 12:12:46 +0900

    [Silver III] Title: 퇴사, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.cc
+++ 백준/Silver/14501. 퇴사/퇴사.cc
@@ -1,25 +1,20 @@
 #include<iostream>
 #include<algorithm>
-#include<vector>
 #include<cmath>
 using namespace std;
 
-static vector<int> D, T, P;
-
 int main(){
     int n;
-
+    int T[16] = {0, };
+    int P[16] = {0, };
+    int D[18] = {0, };
     cin >> n;
 
-    D.resize(n+2);
-    T.resize(n+1);
-    P.resize(n+1);
-    
     for(int i=1; i<=n; i++){
         cin >> T[i] >> P[i];
     }
 
-    for(int i=n; i>0; i--){
+    for(int i=n; i>=1; i--){
         // 퇴사일을 넘을 경우
         if(i+T[i] > n+1){
             D[i] = D[i+1];
