2c04cd8a60f733f63604d773a58e9776b52d53e8
sooobee <20subi@jbnu.ac.kr>
2024-06-20 01:42:14 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2036 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -27,22 +27,22 @@ int main(){
         cin >> d[i][1] >> d[i][2] >> d[i][3];
     }
 
-    for(int i=1; i<n+1; i++){
-        if(i == 1) continue; // 첫번째 집
+    for(int i=n; i>0; i--){
+        if(i == n) continue; // 첫번째 집
 
         else{
             // 2번째 집부터 red가 지어질 때, 전 집이 green, blue 중 작은 것이어야함
-            d[i][1] += min(d[i-1][2], d[i-1][3]);
+            d[i][1] += min(d[i+1][2], d[i+1][3]);
 
             // 2번째 집부터 green이 지어질 때, 전 집이 red, blue 중 작은 것이어야함
-            d[i][2] += min(d[i-1][3], d[i-1][1]);
+            d[i][2] += min(d[i+1][3], d[i+1][1]);
             
             // 2번째 집부터 blue가 지어질 때, 전 집이 green, red 중 작은 것이어야함
-            d[i][3] += min(d[i-1][1], d[i-1][2]);
+            d[i][3] += min(d[i+1][1], d[i+1][2]);
         }
     }
 
-    cout << min(min(d[n][1], d[n][2]), d[n][3]);
+    cout << min(min(d[1][1], d[1][2]), d[1][3]);
 
     
 }
