e458399ec0977cd97c61bf7e37958c25b3cae4be
sooobee <20subi@jbnu.ac.kr>
2024-07-13 18:46:07 +0900

    [Gold V] Title: 하노이 탑 이동 순서, Time: 112 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/11729. 하노이 탑 이동 순서/하노이 탑 이동 순서.cc
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+
+// n개의 원판을 a에서 b까지 이동시키는 함수
+void func(int a, int b, int n){
+
+    if(n == 1){
+        cout << a << ' ' << b << '\n';
+        return;
+    }
+
+    func(a, 6-a-b, n-1); 
+    cout << a << ' ' << b << '\n';
+    func(6-a-b, b, n-1);
+
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int k;
+    cin >> k;
+    cout << (1<<k) -1 << '\n';
+
+    func(1, 3, k);
+}
