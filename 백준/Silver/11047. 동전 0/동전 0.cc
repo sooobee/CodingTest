e3593c25d2df15db4a4a30697a41a274262f4ad8
sooobee <20subi@jbnu.ac.kr>
2024-07-15 16:10:38 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,32 +1,29 @@
 #include<iostream>
 #include<algorithm>
-#include<vector>
 using namespace std;
 
-int main() {
-	int N; 
-	int K;
-	int num;
-	int res=0;
-	int arr[11];
+int n, k;
+int num[11];
+int res;
 
-	cin >> N >> K;
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-	// 동전의 종류를 입력받음
-	for (int i = 0; i < N; i++) {
-		cin >> arr[i];
-	}
+    cin >> n >> k;
 
-	num = K;
-	// 오름차순돼 있으므로 큰 수부터 차례로 탐색
-	for (int i = N-1; i > -1; i--) {
+    for(int i=0; i<n; i++){
+        cin >> num[i];
+    }
 
-		if (num >= arr[i]) {
-			res += num/arr[i]; // 몫을 저장
-			num = num%arr[i];// 나머지는 num에 저장
+    for(int i= n-1; i >= 0; i--){
 
-			if (num == 0)
-				cout << res;
-		}	
-	}
+        if(k >= num[i]){
+            int temp;
+            temp = k / num[i];
+            k %= num[i];
+            res += temp;
+        }
+    }
+    cout << res;
 }
