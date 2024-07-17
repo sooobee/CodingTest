a300fc99d0293d7010f066f4ecb5f66ea4f3f5b4
sooobee <20subi@jbnu.ac.kr>
2024-07-17 21:38:10 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.cc
@@ -0,0 +1,42 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<queue>
+using namespace std;
+
+int main(){
+
+    ios::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
+    priority_queue<long long int, vector<long long int>, greater<long long int>> pq;
+    int n, m;
+    long long int res = 0;
+    cin >> n >> m;
+
+    for(int i=0; i<n; i++){
+        long long int num;
+        cin >> num;
+        pq.push(num);
+    }
+
+    while(m--){
+        long long int data1 = pq.top();
+        pq.pop();
+        long long int data2 = pq.top();
+
+        pq.pop();
+
+        long long int new_card = data1 + data2;
+        pq.push(new_card);
+        pq.push(new_card);
+    }
+
+    while(n--){
+        res += pq.top();
+        pq.pop();
+    }
+
+    cout << res;
+}
