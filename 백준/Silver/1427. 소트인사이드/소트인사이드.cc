b2bbd53acd33d3821ad616cab2f1aab98c756583
sooobee <20subi@jbnu.ac.kr>
2024-09-28 11:03:15 +0900

    [Silver V] Title: 소트인사이드, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.cc
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.cc
@@ -3,12 +3,9 @@
 using namespace std;
 
 int main() {
-
-	string str;
-
-	cin >> str; // string형은 한 문자씩 배열에 들어감
-
-	sort(str.begin(), str.end(), greater<char>()); // 내림차순으로 정렬(greater~)
-
-	cout << str;
+    string str;
+    cin >> str;
+    
+    sort(str.begin(), str.end(), greater<char>());
+    cout << str;
 }
