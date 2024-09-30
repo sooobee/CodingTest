0b571dd89b53399289019cd9da47dcf9e50b6ed6
sooobee <20subi@jbnu.ac.kr>
2024-09-30 21:46:14 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,50 @@
+#include <string>
+#include <vector>
+#include <stack>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) {
+    vector<int> answer;
+    vector<int> t;
+    stack<int> st;
+    
+    
+    for(int i = 0; i < progresses.size(); i++){
+        int num;
+        
+        if((100 - progresses[i]) % speeds[i] > 0){
+            num = (100 - progresses[i]) / speeds[i] + 1;
+        } else {
+            num = (100 - progresses[i]) / speeds[i]; // 남은 시간    
+        }
+
+        t.push_back(num);
+    }
+    
+    int cnt = 0;
+    int max_num = t[0];
+    for(int i = 0; i < t.size(); i++){
+        // 비어 있을 경우 일단 넣음
+        if(t[i] <= max_num){
+                st.push(t[i]);
+                cnt++;
+        } 
+        else { // 더 크면 cnt를 answer에 저장, cnt리셋
+            answer.push_back(cnt); 
+            cnt = 0;
+        
+            st.push(t[i]);
+            max_num = t[i];
+            cnt++;
+        }
+    }
+
+    
+    if(cnt > 0){
+        answer.push_back(cnt);
+    }
+    
+    return answer;
+}
