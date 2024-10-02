a896055ef7b62a0ed988686f1f08040a413c7a17
sooobee <20subi@jbnu.ac.kr>
2024-10-03 01:21:31 +0900

    [level 2] Title: 주식가격, Time: 27.44 ms, Memory: 26.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -0,0 +1,29 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> prices) {
+    vector<int> answer;
+    
+    for(int i=0; i<prices.size(); i++){
+        int cnt = 0;
+        
+        // 마지막 수는 0
+        if(i == prices.size()-1){
+            answer.push_back(0);
+            break;
+        }
+        
+        for(int j=i+1; j<prices.size(); j++){
+            cnt++;
+            
+            if(prices[i] > prices[j]){
+                break;
+            }
+        }
+        answer.push_back(cnt);
+    }
+    
+    return answer;
+}
