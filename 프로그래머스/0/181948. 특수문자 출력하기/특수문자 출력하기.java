ed7480fa77bff3f0fe261dc1d1e0586e0432a408
sooobee <20subi@jbnu.ac.kr>
2024-11-29 20:34:52 +0900

    [level 0] Title: 특수문자 출력하기, Time: 159.71 ms, Memory: 65.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181948. 특수문자 출력하기/특수문자 출력하기.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        // 백슬레시(\) 와 따옴표(", ')의 경우 앞에 \를 붙혀줘야 함
+        System.out.print("!@#$%^&*(\\\'\"<>?:;");
+    }
+}
