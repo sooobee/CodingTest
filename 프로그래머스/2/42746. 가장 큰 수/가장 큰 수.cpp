752bed7b790715bdae51291234a9730a69ddf5a2
sooobee <20subi@jbnu.ac.kr>
2025-02-12 10:43:53 +0900

    [level 2] Title: 가장 큰 수, Time: 63.09 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.cpp
@@ -9,22 +9,24 @@ bool cmp(const string &a, const string &b){
 }
 
 string solution(vector<int> numbers) {
+    vector<string> v2;
     string answer = "";
-    vector<string> v;
     
-    for(int i=0; i<numbers.size(); i++){
-        v.push_back(to_string(numbers[i]));
-    }
-        
-    // 두 수를 조합했을 때 더 큰 것을 앞으로 두는 정렬 수행
-    sort(v.begin(), v.end(), cmp);
-    
-    for(int i=0; i < v.size(); i++){
-        answer += v[i];
+    for(int i = 0; i < numbers.size(); i++){
+        v2.push_back(to_string(numbers[i]));
     }
     
-    if(answer[0] == '0')
+    sort(v2.begin(), v2.end(), cmp);
+    
+    for(int i = 0; i < v2.size(); i++){
+        // cout << v2[i] << " ";
+        answer += v2[i];
+    }
+    
+    // 
+    if(v2[0] == "0"){
         answer = "0";
- 
-    return answer;
+    }
+    
+    return answer;  
 }
