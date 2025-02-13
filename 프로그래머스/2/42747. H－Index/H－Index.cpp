f7e3657ee79cb28afa50b901ac63326757850265
sooobee <20subi@jbnu.ac.kr>
2025-02-13 10:59:28 +0900

    [level 2] Title: H-Index, Time: 2.23 ms, Memory: 4.15 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -5,22 +5,27 @@
 using namespace std;
 
 int solution(vector<int> citations) {
-    int answer = 0;
-    int n = citations.size();
+    int answer;
     
-    // 내림차순 정렬
-    sort(citations.begin(), citations.end(), greater());
-
+    sort(citations.begin(), citations.end(), greater<int>());
     
-    for(int i = 0; i < n; i++){
-        // 인용 수
-        int h = citations[i];
+    // 최대 인용 수 ~ 1까지 돌며 h값을 찾음
+    for(int i = citations[0]; i > 0; i--){
+        int h = i;
+        int countB = 0;
+        int countS = 0;
         
-        // h보다 더 많이 인용된 논문의 수
-        int num = i+1;
+        for(int j = 0; j < citations.size(); j++){
+            // 전체수에서 h보다 큰 수의 개수, 작은 수의 개수를 체크
+            // h: 큰 수의 개수보다 크거나/같, 작은 수의 개수보다 작거나/같
+            if(citations[j] >= h) countB++;
+            else if(citations[j] <= h) countS++;
+        }
         
-        answer = max(answer, min(h, num));
-    
+        if(countB >= h && countS <= h){
+            answer = h;
+            break;
+        }
     }
     return answer;
 }
