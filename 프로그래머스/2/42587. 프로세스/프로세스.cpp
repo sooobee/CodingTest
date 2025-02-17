843def64c5e19bf0ed5f41a282a249f8fe6a0f35
sooobee <20subi@jbnu.ac.kr>
2025-02-17 12:23:49 +0900

    [level 2] Title: 프로세스, Time: 0.02 ms, Memory: 3.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -7,41 +7,35 @@
 using namespace std;
 
 int solution(vector<int> v, int location) {
-    int answer = 0;
-    vector<int> m = v;
-    int num = 0;
-    queue<pair<int, int>> q; // value, index 저장
-    int cnt = 0;
-    int res[101] = {0, };
-    
-    sort(m.begin(), m.end(), greater<int>());
+    int answer = 0; // 방문순서
+    queue<pair<int, int>> q;
     
     for(int i = 0; i < v.size(); i++){
-        q.push({v[i], i});
+        q.push(make_pair(v[i], i)); // 우선순위, 원래 위치
     }
     
+    // 오름차순 정렬
+    sort(v.begin(), v.end(), greater<>());
+    
+    int num = 0;   
     while(!q.empty()){
-        // 우선순위가 작을 경우 뒤에 다시 넣음
-        auto current = q.front();
+        pair<int, int> front = q.front();
+        int max = v[num]; // 우선순위 가장 큰 수
         q.pop();
         
-        // 가장 큰 값인 경우
-        if(current.first == m[num]){
-            cnt++; // 방문순서 +1
-            num++;
+        if(front.first == max){
+            answer++; // 방문순서 + 1
+            num++; // max값 update
             
-            // 탐색중인 index이면 break
-            if(current.second == location){
-                answer = cnt;
+            // 찾던 위치의 수인 경우
+            if(front.second == location){
                 break;
             }
-        } 
-        // 우선 순위가 밀릴 때 다시 큐에 집어넣음
-        else {
-            q.push(current);
+            
+        } else {
+            q.push(front); // 우선순위가 작을 경우, 다시 넣음
         }
-
     }
-
+    
     return answer;
 }
