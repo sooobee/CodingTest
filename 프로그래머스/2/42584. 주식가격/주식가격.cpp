53a18d80f1a37b60535dd83442783ca81ccae39e
sooobee <20subi@jbnu.ac.kr>
2025-03-03 13:00:06 +0900

    [level 2] Title: 주식가격, Time: 27.79 ms, Memory: 26.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/주식가격.cpp
+++ 프로그래머스/2/42584. 주식가격/주식가격.cpp
@@ -4,25 +4,36 @@
 using namespace std;
 
 vector<int> solution(vector<int> prices) {
-    vector<int> answer;
+    vector<int> answer; 
     
-    for(int i=0; i<prices.size(); i++){
+    for(int i = 0; i < prices.size(); i++){
         int cnt = 0;
         
-        // 마지막 수는 0
-        if(i == prices.size()-1){
+        // 마지막 가격인 경우 0 넣고 break
+        if(i == prices.size() - 1){
             answer.push_back(0);
             break;
         }
         
-        for(int j=i+1; j<prices.size(); j++){
-            cnt++;
+        for(int j = i+1; j < prices.size(); j++){
             
+            // 더작은 수가 뒤에 오는 경우 
+            // 카운트 종료
             if(prices[i] > prices[j]){
+                cnt++;
+                answer.push_back(cnt);
                 break;
+                // 같거나 큰 수가 뒤에 오는 경우
+                // 카운트를 더해줌
+            } else {
+                cnt++;
+            }
+            
+            // 탐색 끝난 경우 push
+            if(j == prices.size() - 1){
+                answer.push_back(cnt);
             }
         }
-        answer.push_back(cnt);
     }
     
     return answer;
