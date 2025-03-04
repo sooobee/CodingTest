73fe4f4fdaffd0ea58bf8017a342e6bbfc99b3e0
sooobee <20subi@jbnu.ac.kr>
2025-03-04 16:20:54 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 3.64 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.cpp
+++ 프로그래머스/1/42748. K번째수/K번째수.cpp
@@ -5,24 +5,23 @@
 using namespace std;
 
 vector<int> solution(vector<int> array, vector<vector<int>> commands) {
-    
     vector<int> answer;
-    vector<int> array2;
-    
-    // commands의 수만큼 반복
+
     for(int i = 0; i < commands.size(); i++){
-        array2.clear();
-        
         int start = commands[i][0];
         int end = commands[i][1];
         int target = commands[i][2];
-        
-        // 시작 ~ 끝까지의 수 array2 넣음
-        for(int k = start-1; k < end; k++){
-            array2.push_back(array[k]);
+        vector<int> v;
+
+        // 자른 숫자들을 다시 v에 넣음
+        for(int j = start-1; j < end; j++){
+
+            v.push_back(array[j]);
         }
-        sort(array2.begin(), array2.end());
-        answer.push_back(array2[target-1]);
+        sort(v.begin(), v.end());
+        answer.push_back(v[target-1]);
+
+        
     }
     return answer;
 }
